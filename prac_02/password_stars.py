a98aefcd2e64bf1181e69fdfdd15b946920dc86a
ThomasMehes <thomas.mehes@my.jcu.edu.au>
2023-09-21 12:54:56 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,21 @@
+"""Module docstring"""
 
-minimum_password_length = 8
-password = input("Enter a password: ")
-while len(password) < minimum_password_length:
-    print(f"password must be greater then {minimum_password_length}, try again")
+
+def main():
+    password = get_password()
+    hide_password(password)
+
+
+def get_password():
+    minimum_password_length = 8
     password = input("Enter a password: ")
-print('*' * len(password))
+    while len(password) < minimum_password_length:
+        print(f"password must be greater then {minimum_password_length}, try again")
+        password = input("Enter a password: ")
+    return password
+
+def hide_password(password):
+    print('*' * len(password))
+
+
+main()
