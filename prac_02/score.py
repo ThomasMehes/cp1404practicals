0bcf5bce52b73b16589af46b926a6977d7390315
ThomasMehes <thomas.mehes@my.jcu.edu.au>
2023-09-24 18:32:07 +1000

    incorporated the random score and grade into the program

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,17 +1,33 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+CP1404 - Practical
+Program to determine score status
 """
+import random
+
+def main():
+    """ask the user for their score and print the grade"""
+    score = float(input("Enter score: "))
+    grade = score_category(score)
+    print(f"{grade}")
+
+    random_score = random.randint(0, 100)
+    print(random_score)
+    random_grade = score_category(random_score)
+    print(f"{random_grade}")
+
+def score_category(score):
+    """takes in the user's score as a parameter and returns the result to be printed"""
+    if score < 0:
+        grade = "Invalid score"
+    elif score > 100:
+        grade = "Invalid score"
+    elif score > 90:
+        grade = "Excellent"
+    elif score > 50:
+        grade = "Passable"
+    else:
+        grade = "Bad"
+    return grade
 
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score > 100:
-    print("Invalid score")
-elif score > 90:
-    print("Excellent")
-elif score > 50:
-    print("Passable")
-else:
-    print("Bad")
+main()
