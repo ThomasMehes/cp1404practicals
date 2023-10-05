8bfce12550e3f4bbe388fea795f9c48c9b4ea6e2
ThomasMehes <thomas.mehes@my.jcu.edu.au>
2023-10-05 11:57:54 +1000

    Wasn't sure if I was supposed to remove unnecessary print lines, so they are just commented out.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,21 +9,32 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    print("----------" * 13)  # simply add a line between data and words
+    print_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    total_data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        total_data.append(parts)  # add each formatted list into a larger list
+        # print("----------")
     input_file.close()
+    return total_data
+
+
+def print_data(data):
+    """print data as formatted string in plain english"""
+    for i in range(len(data)):
+        print(f"{data[i][0]} is taught by {data[i][1]} and has {data[i][2]} students")
 
 
 main()
