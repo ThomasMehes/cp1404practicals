2a23770c542fe406e97bda21b4e1db97783171dd
ThomasMehes <thomas.mehes@my.jcu.edu.au>
2023-10-11 19:41:41 +1000

    Completed prac 5 state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,21 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD":"Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+# TOD0: Reformat this file so the dictionary code follows PEP 8 convention
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+# 4. Loop that neatly prints
+max_length = max(len(state) for state in CODE_TO_NAME)
+for state_code in CODE_TO_NAME:
+    print(f"{state_code:3} is {CODE_TO_NAME[state_code]:{max_length}}")
+
+state_code = input("Enter short state: ").upper()  # 3. Allows lowercase inputs to work
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:  # 5. exceptions for EAFP
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+
