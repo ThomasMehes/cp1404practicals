ecc15ed5ba6581cc2238a40831a20828dbc85ff0
ThomasMehes <thomas.mehes@my.jcu.edu.au>
2023-11-06 19:30:05 +1000

    Changes made from PR, credit - DamonLindsay

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,20 +1,20 @@
 """
 CP1404 Practical
-Classes for guitars.py program.
+Classes for myguitars.py program.
 """
 
 
 class Guitar:
     """Methods including overloading operators (__lt__)."""
 
-    def __init__(self, name="", year=0, cost=0):
-        """Initialise ..... """
+    def __init__(self, name="", year=0, cost=0.0):
+        """Initialize the Guitar object."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
-        """Return string formatting. Example: Gibson L-5 CES (1922) : $16,035.40"""
+        """Return string formatting. Example: Gibson L-5 CES, 1922, $16,035.40"""
         return f"{self.name}, {self.year}, {self.cost:,.2f}"
 
     def __lt__(self, other):
