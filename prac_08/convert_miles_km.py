27217b21fd28589e890c1ca74c61be6ecfd72599
ThomasMehes <thomas.mehes@my.jcu.edu.au>
2023-11-09 16:36:19 +1000

    miles to km doctrings

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -13,14 +13,14 @@ class ConvertMilesKmApp(App):
     """ConvertMilesKmApp is a Kivy App for converting miles to kilometres."""
 
     def build(self):
-        """ build the Kivy app from the kv file """
+        """Build the Kivy app from the kv file."""
         Window.size = (600, 300)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_conversion(self, value):
-        """ handle conversion, output result to label widget """
+        """Handle conversion, output result to label widget."""
         try:
             result = float(value) * MILES_TO_KM
             self.root.ids.output_label.text = str(result)
@@ -28,6 +28,7 @@ class ConvertMilesKmApp(App):
             pass
 
     def handle_increment(self, increment):
+        """Handle the up & down button increments of +/- 1."""
         current = float(self.root.ids.input_miles.text)
         self.handle_conversion(current + increment)
         self.root.ids.input_miles.text = str(current + increment)
