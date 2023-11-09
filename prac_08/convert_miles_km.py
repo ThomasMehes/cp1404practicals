3fc715608555f68d7e11e8a9f41fd53c0a1f5253
ThomasMehes <thomas.mehes@my.jcu.edu.au>
2023-11-09 16:34:21 +1000

    Completed miles to km task

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,23 +6,31 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+MILES_TO_KM = 1.60934
+
 
 class ConvertMilesKmApp(App):
+    """ConvertMilesKmApp is a Kivy App for converting miles to kilometres."""
 
     def build(self):
         """ build the Kivy app from the kv file """
         Window.size = (600, 300)
-        self.title = "Square Number"
+        self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
+    def handle_conversion(self, value):
+        """ handle conversion, output result to label widget """
         try:
-            result = float(value) ** 2
+            result = float(value) * MILES_TO_KM
             self.root.ids.output_label.text = str(result)
         except ValueError:
             pass
+
+    def handle_increment(self, increment):
+        current = float(self.root.ids.input_miles.text)
+        self.handle_conversion(current + increment)
+        self.root.ids.input_miles.text = str(current + increment)
 
 
 ConvertMilesKmApp().run()
