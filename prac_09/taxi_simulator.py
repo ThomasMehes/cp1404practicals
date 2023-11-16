97e59d3f06c1ed8b553b726de1ed7d01db2d18ae
ThomasMehes <thomas.mehes@my.jcu.edu.au>
2023-11-16 15:41:25 +1000

    Initial layout and display functionality of taxi sim.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,15 +1,28 @@
-
+from prac_09.car import Car
 from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
+MENU = """q)uit, c)hoose taxi, d)rive"""
 
-taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
 
 def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
 
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            for i, taxi in enumerate(taxis):
+                print(f"{i} - {taxi}")
+        elif choice == "D":
+            print("bees")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
 
-
-
+    print("Thank you.")
 
 
 if __name__ == '__main__':
